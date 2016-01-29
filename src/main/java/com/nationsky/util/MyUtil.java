01724a833cee473a97ee2dbcaa9e23b100eb69de
王国庆 <wangguoqing@nationsky.com>
2016-01-29 11:04:14 +0800

    init

---
+++ src/main/java/com/nationsky/util/MyUtil.java
@@ -0,0 +1,7 @@
+package com.nationsky.util;
+
+/**
+ * Created by wangguoqing on 1/29/2016.
+ */
+public class MyUtil {
+}
---
+++ src/test/java/com/nationsky/test/MyTest.java
@@ -0,0 +1,7 @@
+package com.nationsky.test;
+
+/**
+ * Created by wangguoqing on 1/29/2016.
+ */
+public class MyTest {
+}
